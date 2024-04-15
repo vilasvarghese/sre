1d0420708ee3caaf78639800f546f977347b77f1
vilasvarghese <vilas.varghese@gmail.com>
2024-04-15 12:18:36 +0530

    update repo

--- awsapi/aws_ec2_create_instance.py
+++ awsapi/aws_ec2_create_instance.py
@@ -9,21 +9,53 @@ def create_ec2_instance():
     KeyName="ec2-key" # Change it to the name of the key you have.
     :return: Creates the EC2 instance.
     
-    Amazon ami: ami-05bfbece1ed5beb54
+    Amazon ami: ami-079db87dc4c10ac91 in nvg
     Centos ami: ami-05a36e1502605b4aa
+    Ubuntu ami: ami-0c7217cdde317cfec in ngv
     """
     try:
         print ("Creating EC2 instance")
         resource_ec2 = boto3.client("ec2")
         resource_ec2.run_instances(
-            ImageId="ami-05a36e1502605b4aa",
-            MinCount=1,
-            MaxCount=1,
-            InstanceType="t3a.medium",
-            KeyName="vilas"
+            ImageId="ami-06aa3f7caf3a30282",
+            MinCount=2,
+            MaxCount=2,
+            InstanceType="t3.medium",
+            KeyName="shishir"
         )
     except Exception as e:
         print(e)
+#subro, purvi, sayantan, monalisa, darshan, rahul, william, sayan, mohinder, jyotishman, sankalita, utkarsh, poorna, uday, shishir
+
+def create_ec2_spot_instance():
+    """
+    MaxCount=1, # Keep the max count to 1, unless you have a requirement to increase it
+    InstanceType="t2.micro", # Change it as per your need, But use the Free tier one
+    KeyName="ec2-key" # Change it to the name of the key you have.
+    :return: Creates the EC2 instance.
+    
+    Amazon ami: ami-079db87dc4c10ac91 in nvg
+    Centos ami: ami-05a36e1502605b4aa
+    Ubuntu ami: ami-0c7217cdde317cfec in ngv
+    """
+    try:
+        print ("Creating EC2 instance")
+        resource_ec2 = boto3.client("ec2")
+        resource_ec2.request_spot_instances(
+            LaunchSpecification={
+                'ImageId': 'ami-0e9107ed11be76fde',
+                'KeyName': 'shishir',
+                'InstanceType': 't3a.medium',
+            'Placement': {
+                'AvailabilityZone': 'us-east-1a',
+            }
+
+            #subro, purvi, sayantan, monalisa, darshan, rahul, william, sayan, mohinder, jyotishman, sankalita, utkarsh, poorna
+        }
+        )
+    except Exception as e:
+        print(e)
+    
 
 
 def describe_ec2_instance():
@@ -34,6 +66,51 @@ def describe_ec2_instance():
         return str(resource_ec2.describe_instances()["Reservations"][0]["Instances"][0]["InstanceId"])
     except Exception as e:
         print(e)
+
+
+
+
+def get_all_instances_public_ip():
+ ec2 = boto3.resource('ec2')
+ running_instances = ec2.instances.filter(
+        Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+
+ for instance in running_instances.all():
+     #print(
+     #    "Id: {0}\nPlatform: {1}\nType: {2}\nPublic IPv4: {3}\nAMI: {4}\nState: {5}\n".format(
+     #    instance.id, instance.platform, instance.instance_type, instance.public_ip_address, instance.image.id, instance.state
+     #    )
+     #)
+     instance_name = [tag['Value'] for tag in instance.tags if tag['Key'] == 'Name'][0]
+     print(
+         "Name: {0}, Public IP: {1}\n".format(
+         instance_name, instance.public_ip_address
+         )
+     )
+
+
+
+
+
+def print_all_instances():
+    # Create an EC2 client
+    ec2_client = boto3.client('ec2')
+
+    # Filter for running instances
+    filters = [{'Name': 'instance-state-name', 'Values': ['running']}]
+
+    # Describe all running instances
+    response = ec2_client.describe_instances(Filters=filters)
+
+    # Iterate through instances and print their public IP addresses
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            public_ip_address = instance.get('PublicIpAddress')
+            if public_ip_address:
+                print("Instance ID:", instance['InstanceId'])
+                print("Public IP address:", public_ip_address)
+                print("-----------------------")
+
 
 
 def reboot_ec2_instance():
@@ -75,8 +152,11 @@ def terminate_ec2_instance():
     except Exception as e:
         print(e)
 
-
-create_ec2_instance()
+get_all_instances_public_ip()
+#create_ec2_spot_instance()
+#create_ec2_spot_instance()
+#print_all_instances()
+#create_ec2_instance()
 #describe_ec2_instance()
 #reboot_ec2_instance()
 #stop_ec2_instance()
